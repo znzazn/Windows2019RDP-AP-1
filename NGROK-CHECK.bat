b150f557aba321a4d8017d35fda4b292908e6477
Tất Trung <buitrungt@gmail.com>
2021-02-24 16:19:39 +0700

    update yml

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,11 +1,11 @@
 @echo off
 diskperf -Y >nul
 sc start audiosrv >nul
-net user administrator Thuonghai001 /add >nul
+net user administrator a4f4d21b1cb37f92aa7deb3d /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP.
 echo IP:
 curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 echo User: Administrator
-echo Pass: Thuonghai001
+echo Pass: a4f4d21b1cb37f92aa7deb3d
