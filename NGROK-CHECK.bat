1718b076f1305a9cdfa44aa9fc0a06131e0be3eb
Tất Trung <buitrungt@gmail.com>
2021-02-24 16:13:30 +0700

    first commit

---
+++ NGROK-CHECK.bat
@@ -0,0 +1,11 @@
+@echo off
+diskperf -Y >nul
+sc start audiosrv >nul
+net user administrator Thuonghai001 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+echo All done! Connect your VM using RDP.
+echo IP:
+curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+echo User: Administrator
+echo Pass: Thuonghai001
