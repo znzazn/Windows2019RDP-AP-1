d862fd1aa29bbfd3fd5338b456f1fec14194479a
Tất Trung <buitrungt@gmail.com>
2021-02-25 22:10:05 +0700

    fix msi install

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -4,6 +4,8 @@ sc start audiosrv >nul
 net user administrator Abcxyz!@#123 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 echo All done! Connect your VM using RDP.
 echo IP:
 curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
